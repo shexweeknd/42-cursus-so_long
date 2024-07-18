db1fa63748db4eed7e771d161160fe85f8b2bbc7
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r10s2.42antananarivo.mg>
2024-07-18 11:38:52 +0300

    effectue:     - valgrind sur les test suivants: ./so_long, ./so_long map.ber (avec map col invalide) todo:     - ajustement de get_rows_number.c

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: hramaros <hramaros@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 10:48:44 by hramaros          #+#    #+#             */
-/*   Updated: 2024/07/18 11:29:36 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/07/18 11:36:47 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,8 +93,7 @@ char	**parse_ber(int fd, int rows_nbr, int col_nbr)
 	char	*buffer;
 
 	if (!rows_nbr || !col_nbr)
-		return (close(fd), render_exit((void *)0, "col or row invalids.\n"),
-			NULL);
+		return (close(fd), NULL);
 	result = (char **)malloc(sizeof(char *) * (rows_nbr + 1));
 	if (!result)
 		return (close(fd), NULL);
@@ -122,7 +121,8 @@ int	main(int argc, char **argv)
 		return (1);
 	fd = open_ber(argv[1]);
 	if (argc != 2 || (argc == 2 && !fd))
-		return (mlx_destroy_display(data.mlx), render_exit(NULL, "Error\n"), 0);
+		return (mlx_destroy_display(data.mlx), free(data.mlx), render_exit(NULL,
+				"Error\n"), 0);
 	map = parse_ber(fd, get_rows_nbr(fd), get_col_nbr(fd));
 	if (!map || !map_rules(map))
 		return (mlx_destroy_display(data.mlx), free(data.mlx), render_exit(NULL,
